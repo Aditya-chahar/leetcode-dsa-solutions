994f5faf6143d0b3ffc54ec6e3eaedf14f40ec66
Aditya-chahar <adityachahar48@gmail.com>
2026-09-03 18:49:30 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (29.26%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -4,13 +4,12 @@ public:
         int n = arr.size();
         int low = 0;
         int high = n-1;
-        while(high>=low){
-            int mid = (high+low)/2;
+        while(low<=high){
+            int mid = low + (high-low)/2;
             int missing = arr[mid]-(mid+1);
             if(missing<k) low = mid+1;
             else high = mid-1;
         }
-        return low+k;
-        
+        return low +k;
     }
 };
